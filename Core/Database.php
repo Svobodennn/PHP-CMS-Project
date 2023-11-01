88dd4c6f37a2cacbb1628a718a2387510ca5ff06
Svobodennn <melih.sarac@hotmail.com>
2023-11-01 12:56:09 +0300

    Core Classes, Starter Class

---
+++ Core/Database.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Core;
+
+class Database
+{
+    public $connect;
+
+    public function __construct()
+    {
+        $this->connect = new \PDO('mysql:host=localhost;dbname=todoapp', 'root', '');
+    }
+
+    public function query($sql, $multi = false)
+    {
+        if ($multi == false) {
+            return $this->connect->query($sql, \PDO::FETCH_ASSOC)->fetch() ?? [];
+        } else {
+            return $this->connect->query($sql, \PDO::FETCH_ASSOC)->fetchAll() ?? [];
+        }
+    }
+}
---
+++ Core/Session.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core;
+
+class Session
+{
+    public static function getSession($name){
+        return $_SESSION[$name] ?? false;
+    }
+    public static function setSession($name,$value){
+        $_SESSION[$name] = $value;
+    }
+}
