d572ecfd994a1d485da47654699a1004e5f4f642
Svobodennn <melih.sarac@hotmail.com>
2023-11-02 16:01:48 +0300

    _session method to return session value, _link method to return desired url

--- App/Helpers/app.php
+++ App/Helpers/app.php
@@ -5,3 +5,11 @@ function asset($assetName){
 function redirect($url){
     header('Location:'.URL.$url);
 }
+
+function _link($url = null) {
+    return URL.$url;
+}
+
+function _session($name){
+    return \Core\Session::getSession($name);
+}
