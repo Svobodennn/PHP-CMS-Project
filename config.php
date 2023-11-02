e58a75dafe5540202220671b120f87b78eca4784
Svobodennn <melih.sarac@hotmail.com>
2023-11-02 15:59:08 +0300

    Added database config

--- config.php
+++ config.php
@@ -2,3 +2,8 @@
 session_start();
 const BASEDIR = __DIR__;
 const URL = 'http://localhost/PHP-CMS-Project/';
+
+const HOST = "localhost";
+const DB = "cms";
+const DB_USER = "root";
+const DB_PASSWORD = "";
