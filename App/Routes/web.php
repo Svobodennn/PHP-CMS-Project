b2ae0b7cd579f59ef85b65ed6445716fa8f59316
Svobodennn <melih.sarac@hotmail.com>
2023-11-07 14:08:01 +0300

    Login page middleware deactivated, Customer, Project edit and remove routing.

--- App/Routes/web.php
+++ App/Routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 $cms->router->before('GET|POST', '/', 'Middlewares\MiddlewareAuth@isLogin');
-$cms->router->before('GET|POST', '/login', 'Middlewares\MiddlewareAuth@isLogin');
+//$cms->router->before('GET|POST', '/login', 'Middlewares\MiddlewareAuth@isLogin');
 $cms->router->before('GET|POST', '/customer.*', 'Middlewares\MiddlewareAuth@isLogin');
 $cms->router->before('GET|POST', '/project.*', 'Middlewares\MiddlewareAuth@isLogin');
 
@@ -19,16 +19,20 @@ $cms->router->mount('/customer', function () use ($cms) {
     $cms->router->get('/', 'Controllers\Customer@Index');
     //id
     $cms->router->get('/add', 'Controllers\Customer@Add');
+    $cms->router->post('/add', 'Controllers\Customer@AddCustomer');
     $cms->router->get('/edit/([0-9]+)', 'Controllers\Customer@Edit');
+    $cms->router->post('/edit', 'Controllers\Customer@EditCustomer');
     $cms->router->get('/([0-9]+)', 'Controllers\Customer@Detail');
-    $cms->router->post('/remove/([0-9]+)', 'Controllers\Customer@Remove');
+    $cms->router->post('/remove', 'Controllers\Customer@RemoveCustomer');
 });
 // Projects
 $cms->router->mount('/project', function () use ($cms) {
     $cms->router->get('/', 'Controllers\Project@Index');
     //id
     $cms->router->get('/add', 'Controllers\Project@Add');
+    $cms->router->post('/add', 'Controllers\Project@AddProject');
     $cms->router->get('/edit/([0-9]+)', 'Controllers\Project@Edit');
+    $cms->router->post('/edit', 'Controllers\Project@EditProject');
     $cms->router->get('/([0-9]+)', 'Controllers\Project@Detail');
-    $cms->router->post('/remove/([0-9]+)', 'Controllers\Project@Remove');
+    $cms->router->post('/remove', 'Controllers\Project@RemoveProject');
 });
