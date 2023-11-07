d79c4ea4ebcb51884d0224d63fd1e99ffaec116b
Svobodennn <melih.sarac@hotmail.com>
2023-11-07 14:08:20 +0300

    Add Project function

--- App/Controllers/Project.php
+++ App/Controllers/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Model\ModelCustomer;
 use Core\BaseController;
 
 class Project extends BaseController
@@ -18,6 +19,37 @@ class Project extends BaseController
         $data['sidebar'] = $this->view->load('static/sidebar');
         echo $this->view->load('project/add',compact('data')); // ['data' => $data]
     }
+
+    public function AddProject()
+    {
+        $data = $this->request->post();
+
+        if (!$data['projectName']){
+            $status = 'error';
+            $title = 'Oops!';
+            $msg = "Please enter project name";
+            echo json_encode(['status' => $status, 'title' => $title, 'msg' => $msg]);
+            exit();
+        }
+
+        $model = new ModelProject();
+        $result = $model->addProject($data);
+
+        if ($result){
+            $status = 'success';
+            $title = 'Yay!';
+            $msg = 'Project added';
+            echo json_encode(['status' => $status, 'title' => $title, 'msg' => $msg]);
+            exit();
+        } else {
+            $status = 'error';
+            $title = 'Oops!';
+            $msg = 'Something went wrong';
+            echo json_encode(['status' => $status, 'title' => $title, 'msg' => $msg]);
+            exit();
+        }
+    }
+
     public function Edit($id)
     {
         $data['navbar'] = $this->view->load('static/navbar');
