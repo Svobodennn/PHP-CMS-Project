369d524f07b4177077f546268de84a7f28da08ae
Svobodennn <melih.sarac@hotmail.com>
2023-11-09 15:36:15 +0300

    Home controller

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -2,12 +2,26 @@
 
 namespace App\Controllers;
 
+use App\Model\ModelCustomer;
+use App\Model\ModelHome;
+use App\Model\ModelProject;
 use Core\BaseController;
 
 class Home extends BaseController
 {
     public function index()
     {
+
+        $model = new ModelHome();
+        $data['total'] = $model->getEverything()['total'];
+        $data['projects'] = $model->getEverything()['project'];
+
+        $modelProject = new ModelProject();
+        $data['projects_table'] = $modelProject->getProjectsByStatus('a');
+
+        $modelCustomer = new ModelCustomer();
+        $data['customers_table'] = $modelCustomer->getCustomers(5);
+
         $data['navbar'] = $this->view->load('static/navbar');
         $data['sidebar'] = $this->view->load('static/sidebar');
         echo $this->view->load('home/index',compact('data')); // ['data' => $data]
